139c47a5c9a971f0448dac81114982a48a53376f
Erik Golinelli <erik@codekraft.it>
2023-11-20 09:39:18 +0100

    Recap and Score components

---
+++ src/components/Recap.tsx
@@ -0,0 +1,26 @@
+import {getBrowser, getBrowserVersion, getDevice, getDeviceType, getOS, getOSVersion} from "../utils";
+import './Recap.css'
+
+function Recap() {
+    const Browser = getBrowser()
+    const OS = getOS()
+    const Device = getDevice()
+    const BrowserVersion = getBrowserVersion()
+    const OSVersion = getOSVersion()
+    const DeviceType = getDeviceType()
+    return  (
+        <div id={'recap'}>
+            <ul>
+                <li><label>User Agent::</label> {window.navigator.userAgent}</li>
+                <li><label>Browser:</label> {Browser}</li>
+                <li><label>OS:</label> {OS}</li>
+                <li><label>Device:</label> {Device}</li>
+                <li><label>BrowserVersion:</label> {BrowserVersion}</li>
+                <li><label>OSVersion:</label> {OSVersion}</li>
+                <li><label>DeviceType:</label> {DeviceType}</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Recap
---
+++ src/components/Score.tsx
@@ -0,0 +1,5 @@
+function Score(props : {score: string}) {
+    return <p className="align-center score">Test Passed: {props.score}%</p>;
+}
+
+export default Score
