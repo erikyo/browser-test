119b6a79de204da21cfedfc1893ad897c30a9b6a
gardenboi <dawidgrden.99@gmail.com>
2023-11-22 19:09:02 +0100

    added pseudo queue for async tests

--- src/utils/fe-functions.ts
+++ src/utils/fe-functions.ts
@@ -25,13 +25,15 @@
   export function checkSecuritiesByCategories(props: ObjTest): Promise<ObjResult> | ObjResult {
     const newDataTableResults: ObjResult = {};
   
+    let i = 0;
     for (const categoryName of Object.keys(props)) {
+      
       const categoryTests = props[categoryName];
       newDataTableResults[categoryName] = [];
   
       let promiseResolved = false;
-      const pendingPromises: [string, Promise<any>][] = [];
-  
+      const pendingPromises: [string, number , Promise<any>][] = [];
+      
       for (const test of categoryTests) {
         const { testName, test: check } = test;
         const result = check ? '☑️' : '📛';
@@ -41,35 +43,42 @@
         // Handle non-promise values immediately
         if (!isPromise) {
           newDataTableResults[categoryName].push({
-            id: newDataTableResults[categoryName].length,
+            id: i,
             testName,
             value: test.value ?? !! check,
             result: typeof result !== 'object' ? result : JSON.stringify(result),
           });
         } else {
           // Track pending promises
-          pendingPromises.push([testName, test.value]);
+          pendingPromises.push([testName, i , test.value]);
   
           // Delay processing of subsequent values until the promise resolves
           if (!promiseResolved) {
             promiseResolved = true;
   
             (async () => {
-              for (const [testName, promise] of pendingPromises) {
-                const resolvedResult = await promise;
-  
-                newDataTableResults[categoryName].push({
-                  id: newDataTableResults[categoryName].length,
-                  testName,
-                  value: resolvedResult ?? !! check,
-                  result: typeof resolvedResult !== 'object' ? resolvedResult : JSON.stringify(resolvedResult),
-                });
-              }
+                for (const [testName, index, promise] of pendingPromises) {
+                  const resolvedResult = await promise;
+            
+                  insertAt(newDataTableResults[categoryName], index,
+                    {
+                      id: index,
+                      testName,
+                      value: resolvedResult ?? !! check,
+                      result: result ? '☑️' : '📛',
+                    }
+                    );
+                }
             })();
           }
         }
+        i++;
       }
     }
-  
     return newDataTableResults;
   }
+
+function insertAt<T, K extends T >(array: T[], index: number, elementsArray: K): T[] {
+  array.splice(index, 0, elementsArray);
+  return array;
+}
