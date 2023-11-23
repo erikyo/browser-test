eb730ea4f46d3f25805d0f3deab75b62780d7cf3
Erik Golinelli <erik@codekraft.it>
2023-11-23 10:45:27 +0100

    enhancing main page load

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,5 +1,5 @@
 function Score(props : {score: string}) {
-    return props.score ? <p className="text-2xl font-bold text-center score">Test Passed: {props.score}%</p> : null;
+    return props.score ? <p className="text-2xl font-bold text-center score">Test Failed: {(100 - props.score).toFixed(2)}%</p> : null;
 }
 
 export default Score
